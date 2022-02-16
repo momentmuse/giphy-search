889ed3451e17a182814a26daa1edaf4b66f84fe6
Amy Kirasack <amykirasack@gmail.com>
2022-02-16 17:03:10 +0000

    feat: page location preliminary logic

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -9,10 +9,12 @@ export const SearchContainer = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResultData, setSearchResultData] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
+  const [paginationImageCount, setPaginationImageCount] = useState(0);
 
   const fetchResults = () => {
     // send api GET request with searchQuery, limit = interval and offset = currentPage * interval
     // set searchResultData
+    // set pagImgCount (pag object comes with data)
   };
 
   const handleChange = (e) => {
@@ -26,12 +28,27 @@ export const SearchContainer = () => {
     console.log('handling SUBMIT!! ', searchQuery);
   };
 
+  const handlePageChange = (value) => {
+    setCurrentPage(currentPage + value);
+  };
+
+  const totalPages = () => {
+    // calculate num of pages by dividing total num of image results by 10 (interval constant) and any leftover is an extra page
+    // Math.ceil?
+    return 10;
+  };
+
   return (
     <>
       <h2>I'm the Search Container!</h2>
       <SearchInput handleChange={handleChange} handleSubmit={handleSubmit} />
       <SearchResults />
-      <SearchPageLocation />
+      <SearchPageLocation
+        /*currentPage + 1 because it starts at 0 for offset*/
+        currentPage={currentPage + 1}
+        handlePageChange={handlePageChange}
+        totalPages={totalPages()}
+      />
     </>
   );
 };
--- src/components/SearchPageLocation.js
+++ src/components/SearchPageLocation.js
@@ -1,9 +1,21 @@
-export const SearchPageLocation = () => {
+export const SearchPageLocation = ({
+  currentPage,
+  totalPages,
+  handlePageChange,
+}) => {
+  const isDisabled = (value) => {
+    return currentPage + value === 0 || currentPage + value > totalPages;
+  };
+
   return (
     <>
-      <button>prev</button>
-      page x of total
-      <button>next</button>
+      <button onClick={() => handlePageChange(-1)} disabled={isDisabled(-1)}>
+        prev
+      </button>
+      page {currentPage} of {totalPages}
+      <button onClick={() => handlePageChange(1)} disabled={isDisabled(1)}>
+        next
+      </button>
     </>
   );
 };
